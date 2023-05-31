84b307f0b77ad4f54481e2768b8dd3d609708c3d
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-05-31 10:08:24 -0400

    LC15 added

--- Solutions/LC15.py
+++ Solutions/LC15.py
@@ -1,24 +1,30 @@
-#15. 3Sum
-
 class Solution:
     def threeSum(self, nums):
-        nums = sorted(nums)
-        lis = []
-        for i in range(len(nums)):
 
-            l,r = i + 1, len(nums) - 1
+        result = []
+        nums.sort()
 
-            if i > 0 and nums[i] == nums[i-1]:
+        for i, a in enumerate(nums):
+
+            if i > 0 and a == nums[i - 1]:
                 continue
 
+            l, r = i + 1, len(nums) - 1
+
             while l < r:
-                if nums[i] + nums[l] + nums[r] > 0:
+
+                threeSum = a + nums[l] + nums[r]
+
+                if threeSum > 0:
                     r -= 1
-                elif nums[i] + nums[l] + nums[r] < 0:
+
+                elif threeSum < 0:
                     l += 1
+
                 else:
-                    lis.append([nums[i], nums[l], nums[r]])
+                    result.append([a, nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        return lis
+
+        return result

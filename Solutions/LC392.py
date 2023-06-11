ad314559116bb3dfe726d541d3792dfcd32e5721
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-06-11 10:57:31 -0400

    LC392 Improvised

--- Solutions/LC392.py
+++ Solutions/LC392.py
@@ -1,27 +1,13 @@
-s = "aaaaaa"
-t = "bbaaaa"
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
 
-count = 0
-j = 0
-i = 0
+        i, j = 0, 0
 
-while i < len(s):
-    while j < len(t):
-        if s[i] == t[j]:
-            count += 1
-            i += 1
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            
             j += 1
-            break
         
-        j += 1
-
-    if j == len(t):
-        break
-    
-    print(i, j)
-
-if count == len(s):
-    print("Y")
-
-else:
-    print("F")
+        return True if i == len(s) else False

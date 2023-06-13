9a0a16ac95b4d97407f435eb4ba0f0876369e514
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-06-13 15:00:03 -0400

    LC14 improvised

--- Solutions/LC14.py
+++ Solutions/LC14.py
@@ -1,19 +1,12 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs) -> str:
         
-        result = strs[0]
+        result = ""
 
-        for i in range(1, len(strs)):
-            if result == "":
-                break  
-            count = 0
-            t = list(zip(result, strs[i]))
-            for i in range(len(t)):
-                if t[i][0] == t[i][1]:
-                    count += 1
-                else:
-                    break
-
-            result = result[:count] 
-
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return result
+            result += strs[0][i]
+        
         return result

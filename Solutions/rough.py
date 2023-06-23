4d747631de324101056f9fe9817a201b32b8ad80
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-06-23 14:58:05 -0400

    LC146 added

--- Solutions/rough.py
+++ Solutions/rough.py
@@ -1,7 +1,15 @@
-n = 999999
-mul = 1
+l = []
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        l.append("FizzBuzz")
+    
+    elif i % 5 == 0:
+        l.append("Fizz")
+    
+    elif i % 3 == 0:
+        l.append("Buzz")
+    
+    else:
+        l.append(i)
 
-for i in range(n, 0, -1):
-    mul *= n
-
-print(mul)
+print(l)

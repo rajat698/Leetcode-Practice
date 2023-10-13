932c84a60f733b7ca0e98d297d01db5a43f9f86b
rajat698 <emailofrajatyadav@gmail.com>
2023-10-13 17:28:19 -0400

    LC 150 added

--- Solutions/rough.py
+++ Solutions/rough.py
@@ -1,18 +1,1 @@
-n = 58
-
-#4, 90, 900, 0
-#1990, 1900, 1000
-
-l = []
-x = 10
-while n != 0:
-    
-    a = n % x
-    x *= 10
-    l.append(a)
-
-    n -= a
-
-l.sort(reverse=True)
-
-print(l)
+print(int(6/-132))
